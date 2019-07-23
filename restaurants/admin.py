798dd41fccb5aa35b9226f9c987a8a8dd0cb4cea
rana <ranonah222@gmail.com>
2019-07-23 15:34:58 +0300

    task4 sulotion

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Resturant
 
 # Register your models here.
+admin.site.register(Resturant)
--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
+
+class Resturant(models.Model):
+
+	name=models.CharField(max_length=20)
+	description=models.TextField()
+	opening_time =models.CharField(max_length=40)
+	closing_time=models.CharField(max_length=40)
+	  #def __str__(self) :
+      
+
+      #return self.name
 # Create your models here.
+ 
+
+# Create your models here.
+   
+
+
