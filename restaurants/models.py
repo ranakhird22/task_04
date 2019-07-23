cf5591548bd9a5fb4c1df059767e566842c013ac
rana <ranonah222@gmail.com>
2019-07-23 16:38:07 +0300

    fix type

--- restaurants/models.py
+++ restaurants/models.py
@@ -6,8 +6,9 @@ class Resturant(models.Model):
 
 	name=models.CharField(max_length=20)
 	description=models.TextField()
-	opening_time =models.CharField(max_length=40)
-	closing_time=models.CharField(max_length=40)
+	opening_time =models.TimeField(max_length=40)
+	closing_time=models.TimeField(max_length=40)
+
 	def __str__(self):
          return self.name
 # Create your models here.
